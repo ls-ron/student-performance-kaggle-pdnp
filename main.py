bc409286ef34946a726eabf1890d262f9b4cbb9a
ls-ron <rony24th@gmail.com>
2024-07-07 00:12:48 +0300

    added summary points and refined code

--- main.py
+++ main.py
@@ -18,9 +18,14 @@ for i in range(len(spotify_df)):
     else:
         continue
 
-mean_girl_gpa = round(total_gpa_f / num_girls, 5)
-print(mean_girl_gpa)
-mean_boy_gpa = round(total_gpa_m / num_boys, 5)
-print(mean_boy_gpa)
+mean_girl_gpa = round(total_gpa_f / num_girls, 3)
+mean_boy_gpa = round(total_gpa_m / num_boys, 3)
+print(f"The average male's GPA was {mean_boy_gpa}, while the average female's GPA was {mean_girl_gpa}")
 
+gender_arr = np.array(spotify_df['Gender'])
+print(f"for every 100 students, {round(100 * np.mean(gender_arr))} will be male")
+
+absences = np.array(spotify_df['Absences'])
+mean_absences = round(np.mean(absences))
+print(f'There are {np.sum(absences)} total absences with an average of {mean_absences} for each student')
 
